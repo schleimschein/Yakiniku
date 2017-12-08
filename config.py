7fb116a85a2ebf1983c327200ff364e4710ebf57
schleimschein <sbstnstern@gmail.com>
2017-12-08 12:50:08 +0100

    Changed database path export variable to unique name

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = "31t158yuaj2289iusysxd987as8cqjgkl3p97jsbtxsaq"
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = os.environ.get("TRUNKS_DATABASE_URL")
